3e69db8665a445efad847ba917e3e8aaca2fba2a
Administrator <temomeshveliani777@gmail.com>
2025-11-10 00:52:52 +0400

    Made some changes, added go keywords after each query block

--- schema.sql
+++ schema.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE University_Management_System;
-
+GO
 USE University_Management_System;
+GO
 
 CREATE TABLE Departments
 (
@@ -9,7 +10,7 @@ CREATE TABLE Departments
 	DepartmentHead NVARCHAR(100) not null ,
 	CONSTRAINT PK_DepartmentID PRIMARY KEY (DepartmentID)
 )
-
+GO
 CREATE TABLE Professors
 (
 	ProfessorID INT IDENTITY(1000,1), --PRIMARY KEY
@@ -18,10 +19,10 @@ CREATE TABLE Professors
 	Phone NVARCHAR(15) not null, 
 	DepartmentID INT not null, --FOREIGN KEY
 	CONSTRAINT PK_ProfessorID PRIMARY KEY (ProfessorID),
-	CONSTRAINT FK_DepartmentID FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
-	CONSTRAINT CH_Email CHECK (Email like '%@%')
+	CONSTRAINT FK_Professors_DepartmentID FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID),
+	CONSTRAINT CH_Professors_Email CHECK (Email like '%@%')
 )
-
+GO
 CREATE TABLE Students 
 (
 	StudentID INT IDENTITY(1000,1),  --PRIMARY KEY
@@ -31,11 +32,11 @@ CREATE TABLE Students
 	DepartmentID INT not null , --FOREIGN KEY 
 	EnrollmentYear INT DEFAULT(YEAR(GETDATE())), --CHECK
 	CONSTRAINT PK_StudentID PRIMARY KEY(StudentID) ,
-	CONSTRAINT FK_DepartmentID FOREIGN KEY ( DepartmentID) REFERENCES Departments(DepartmentID),
-	CONSTRAINT CH_Email CHECK (Email like  '%@%') ,
-	CONSTRAINT CH_EnrollmentYear CHECK (EnrollmentYear<=YEAR(GETDATE()))
+	CONSTRAINT FK_Students_DepartmentID FOREIGN KEY ( DepartmentID) REFERENCES Departments(DepartmentID),
+	CONSTRAINT CH_Students_Email CHECK (Email like  '%@%') ,
+	CONSTRAINT CH_Students_EnrollmentYear CHECK (EnrollmentYear<=YEAR(GETDATE()))
 )
-
+GO
 CREATE TABLE Courses 
 (
 	CourseID INT IDENTITY(1000,1), --PRIMARY KEY
@@ -44,19 +45,19 @@ CREATE TABLE Courses
 	Credits INT not null, 
 	DepartmentID INT not null , --FOREIGN KEY 
 	CONSTRAINT PK_CourseID PRIMARY KEY(CourseID) , 
-	CONSTRAINT FK_DepartmentID FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
+	CONSTRAINT FK_Courses_DepartmentID FOREIGN KEY (DepartmentID) REFERENCES Departments(DepartmentID)
 
 )
-
+GO
 CREATE TABLE Prerequisites
 ( 
 	CourseID INT not null, --FOREIGN KEY
 	PrerequisiteCourseID INT not null, --FOREIGN KEY 
-	CONSTRAINT FK_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID), 
-	CONSTRAINT FK_PrerequisiteCourseID FOREIGN KEY(PrerequisiteCourseID) REFERENCES Courses(CourseID),
+	CONSTRAINT FK_Prerequisites_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID), 
+	CONSTRAINT FK_Prerequisites_PrerequisiteCourseID FOREIGN KEY(PrerequisiteCourseID) REFERENCES Courses(CourseID),
 	CONSTRAINT PK_Prerequisites PRIMARY KEY (CourseID,PrerequisiteCourseID)
 )
-
+GO
 CREATE TABLE Enrollments 
 (
 	EnrollmentID INT IDENTITY(1000,1) , --PRIMARY KEY
@@ -65,12 +66,12 @@ CREATE TABLE Enrollments
 	EnrollmentDate DATE DEFAULT(GETDATE()), --CHECK
 	Grade INT not null, --CHECK
 	CONSTRAINT PK_EnrollmentID PRIMARY KEY (EnrollmentID), 
-	CONSTRAINT FK_StudentID FOREIGN KEY (StudentID) REFERENCES Students(StudentID), 
-	CONSTRAINT FK_CourseID FOREIGN KEY (CourseID) REFERENCES Courses(CourseID), 
-	CONSTRAINT CH_EnrollmentDate CHECK (EnrollmentDate <= GETDATE()),
-	CONSTRAINT CH_Grade CHECK (Grade>=0 AND Grade <=100)
+	CONSTRAINT FK_Enrollments_StudentID FOREIGN KEY (StudentID) REFERENCES Students(StudentID), 
+	CONSTRAINT FK_Enrollments_CourseID FOREIGN KEY (CourseID) REFERENCES Courses(CourseID), 
+	CONSTRAINT CH_Enrollments_EnrollmentDate CHECK (EnrollmentDate <= GETDATE()),
+	CONSTRAINT CH_Enrollments_Grade CHECK (Grade>=0 AND Grade <=100)
 )
-
+GO
 CREATE TABLE CourseSchedule
 (
 	ScheduleID INT IDENTITY(1000,1) , --PRIMARY KEY
@@ -81,8 +82,9 @@ CREATE TABLE CourseSchedule
 	DayOfWeek NVARCHAR(20) not null ,  --CHECK
 	Time Time not null ,
 	CONSTRAINT PK_ScheduleID PRIMARY KEY(ScheduleID) , 
-	CONSTRAINT FK_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID), 
-	CONSTRAINT FK_ProfessorID FOREIGN KEY(ProfessorID) REFERENCES Professors(ProfessorID),
-	CONSTRAINT CH_DayOfWeek CHECK (LOWER(DayOfWeek) in ('monday','tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')),
-	CONSTRAINT CH_Semester CHECK (Semester IN (1, 2, 3, 4, 5, 6, 7, 8))
-)	
+	CONSTRAINT FK_CourseSchedule_CourseID FOREIGN KEY(CourseID) REFERENCES Courses(CourseID), 
+	CONSTRAINT FK_CourseSchedule_ProfessorID FOREIGN KEY(ProfessorID) REFERENCES Professors(ProfessorID),
+	CONSTRAINT CH_CourseSchedule_DayOfWeek CHECK (LOWER(DayOfWeek) in ('monday','tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')),
+	CONSTRAINT CH_CourseSchedule_Semester CHECK (Semester IN (1, 2, 3, 4, 5, 6, 7, 8))
+)
+GO
